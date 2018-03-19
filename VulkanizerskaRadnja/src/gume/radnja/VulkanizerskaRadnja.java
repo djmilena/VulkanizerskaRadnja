0618a219b51380a0d34fff01983c8dd65032379b
djordjevic6016 <mimi.djordjevic97@gmail.com>
2018-03-19 17:15:02 +0100

    Dokumentovana klasa VulkanizerskaRadnja

--- VulkanizerskaRadnja/src/gume/radnja/VulkanizerskaRadnja.java
+++ VulkanizerskaRadnja/src/gume/radnja/VulkanizerskaRadnja.java
@@ -4,10 +4,26 @@ import java.util.LinkedList;
 
 import gume.AutoGuma;
 
+/**
+ * Klasa koja predstavlja vulkanizersku radnju.
+ * @author Milena Djordjevic
+ * @version 1.0
+ */
+
 public class VulkanizerskaRadnja {
 	
+	/**
+	 * Lista objekata klase AutoGuma.
+	 */
 	private LinkedList<AutoGuma> gume = new LinkedList<AutoGuma>();
-
+	
+	/**
+	 * Dodaje gumu na pocetku liste.
+	 * @param a klase AutoGuma
+	 * 
+	 * @throws java.lang.NullPointerException.class ukoliko je a = null.
+	 * @throws java.lang.RuntimeException.class ukoliko lista vec sadrzi objekat a.
+	 */
 	public void dodajGumu(AutoGuma a) {
 		if (a == null)
 			throw new NullPointerException("Guma ne sme biti null");
@@ -15,7 +31,31 @@ public class VulkanizerskaRadnja {
 			throw new RuntimeException("Guma vec postoji");
 		gume.addFirst(a);
 	}
+	/**
+	 * Vraca listu gume.
+	 * @return gume kao listu objekata klase AutoGuma
+	 */
+	public LinkedList<AutoGuma> getGume() {
+		return gume;
+	}
+	
+	/**
+	 * Postavlja listu novu vrednost liste gume.
+	 * @param gume lista objekata klase AutoGuma
+	 */
+	public void setGume(LinkedList<AutoGuma> gume) {
+		
+		this.gume = gume;
+	}
 
+	/**
+	 * Pronalazi i vraca sve objekate sa zadatim modelom, u vidu nove liste.
+	 * Ukoliko je parametar null metoda vraca null.
+	 *  
+	 * @param markaModel kao atribut klase AutoGuma
+	 * 
+	 * @return novaLista kao lista objekata klase AutoGuma
+	 */
 	public LinkedList<AutoGuma> pronadjiGumu(String markaModel) {
 		if (markaModel == null)
 			return null;
@@ -25,5 +65,4 @@ public class VulkanizerskaRadnja {
 				novaLista.add(gume.get(i));
 		return novaLista;
 	}
-	
 }
